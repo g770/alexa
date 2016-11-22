f9552420fff7e2882ce4b314504494535bf7d990
g770 <g770@users.noreply.github.com>
2016-11-21 20:08:05 -0700

    Multiplication quiz
    Simple alexa skill that quizzes you on multiplication tables

---
+++ multiplication quiz/src/index.js
@@ -0,0 +1,282 @@
+
+'use strict';
+
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        if (event.session.application.applicationId !== "amzn1.ask.skill.46a1b2eb-2916-477c-85f3-ef999509ef75") {
+            context.fail("Invalid Application ID");
+        }
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // add any session init logic here
+}
+
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // handle yes/no intent after the user has been prompted
+    if (session.attributes && session.attributes.userPromptedToContinue) {
+        delete session.attributes.userPromptedToContinue;
+        if ("AMAZON.NoIntent" === intentName) {
+            handleFinishSessionRequest(intent, session, callback);
+        } else if ("AMAZON.YesIntent" === intentName) {
+            handleRepeatRequest(intent, session, callback);
+        }
+    }
+
+    // dispatch custom intents to handlers here
+    if ("AnswerIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AnswerOnlyIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("DontKnowIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.YesIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.NoIntent" === intentName) {
+        handleAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.StartOverIntent" === intentName) {
+        getWelcomeResponse(callback);
+    } else if ("AMAZON.RepeatIntent" === intentName) {
+        handleRepeatRequest(intent, session, callback);
+    } else if ("AMAZON.StopIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else if ("AMAZON.CancelIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else {
+        throw "Invalid intent";
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // Add any cleanup logic here
+}
+
+// ------- Skill specific business logic -------
+
+var CARD_TITLE = "Multiplication"; // Be sure to change this for your skill.
+
+function getWelcomeResponse(callback) {
+    var sessionAttributes = {};
+    var speechOutput = "Let's practice some multiplication.  I will ask you questions, try to get as many right as you can.  ";
+    var shouldEndSession = false;
+
+    var question = generateQuestion();
+    speechOutput = speechOutput + question.question;
+
+    sessionAttributes = {
+        "speechOutput": speechOutput,
+        "repromptText": speechOutput,
+        "question": question,
+    };
+    callback(sessionAttributes,
+        buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, shouldEndSession));
+}
+
+function generateQuestion() {
+
+    var int1 = getRandomIntInclusive(0, 12);
+    var int2 = getRandomIntInclusive(0, 12);
+    var questionSpeech = "What is " + int1 + " times " + int2 + "?";
+    var answer = int1 * int2;
+
+    var gameQuestion = {
+        question: questionSpeech,
+        answer: answer
+    };
+
+    return gameQuestion;
+}
+
+
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function handleAnswerRequest(intent, session, callback) {
+    var speechOutput = "";
+    var sessionAttributes = {};
+    var gameInProgress = session.attributes && session.attributes.question;
+    var answerSlotValid = isAnswerSlotValid(intent);
+    var userGaveUp = intent.name === "DontKnowIntent";
+
+    if (!gameInProgress) {
+        // If the user responded with an answer but there is no game in progress, ask the user
+        // if they want to start a new game. Set a flag to track that we've prompted the user.
+        sessionAttributes.userPromptedToContinue = true;
+        speechOutput = "There is no game in progress. Do you want to start a new game? ";
+        callback(sessionAttributes,
+            buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, false));
+    } else if (!answerSlotValid && !userGaveUp) {
+        // If the user provided answer isn't a number 
+        // return an error message to the user. Remember to guide the user into providing correct values.
+        var reprompt = session.attributes.speechOutput;
+        var speechOutput = "Your answer wasn't a number, please try again";
+        callback(session.attributes,
+            buildSpeechletResponse(CARD_TITLE, speechOutput, reprompt, false));
+    } else {
+        // Normal case
+        var gameQuestion = session.attributes.question;
+        var correctAnswer = gameQuestion.answer;
+
+        var speechOutputAnalysis = "";
+
+        if (answerSlotValid && parseInt(intent.slots.Answer.value) == correctAnswer) {
+            speechOutputAnalysis = "correct. ";
+        } else {
+            if (!userGaveUp) {
+                speechOutputAnalysis = "wrong. "
+            }
+            speechOutputAnalysis += "The correct answer is " + correctAnswer + ". ";
+        }
+
+        speechOutputAnalysis += "Let's try another one.  ";
+        var newQuestion = generateQuestion();
+
+        var speechOutput = speechOutputAnalysis + newQuestion.question;
+        sessionAttributes.question = newQuestion;
+        sessionAttributes.speechOutput = speechOutput;
+        sessionAttributes.repromptText = speechOutput;
+
+        callback(sessionAttributes,
+            buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, false));
+    }
+}
+
+function handleRepeatRequest(intent, session, callback) {
+    // Repeat the previous speechOutput and repromptText from the session attributes if available
+    // else start a new game session
+    if (!session.attributes || !session.attributes.speechOutput) {
+        getWelcomeResponse(callback);
+    } else {
+        callback(session.attributes,
+            buildSpeechletResponseWithoutCard(session.attributes.speechOutput, session.attributes.repromptText, false));
+    }
+}
+
+function handleFinishSessionRequest(intent, session, callback) {
+    // End the session with a "Good bye!" if the user wants to quit the game
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Good bye!", "", true));
+}
+
+function isAnswerSlotValid(intent) {
+    var answerSlotFilled = intent.slots && intent.slots.Answer && intent.slots.Answer.value;
+    var answerSlotIsInt = answerSlotFilled && !isNaN(parseInt(intent.slots.Answer.value));
+    return answerSlotIsInt;
+}
+
+// ------- Helper functions to build responses -------
+
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
+
